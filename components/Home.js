d8786552fd2f6f2dd5c57a38a80d1598833c2838
Max <maxime.leb@gmail.com>
2023-02-27 12:58:46 +0100

    warning message for screens

--- components/Home.js
+++ components/Home.js
@@ -2,10 +2,10 @@ import styles from '../styles/Home.module.scss'
 import Header from './Header'
 import AddNote from './AddNote'
 import Note from './Note'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 
-function Home() {
+export default function Home() {
 
   const notes = useSelector(state => state.notes.value)
   const modes = useSelector(state => state.modes.value)
@@ -30,15 +30,43 @@ function Home() {
     />
   ))
 
-  return (
-    <div className={`${modes.darkMode && 'darkMode'}`} >
-      <main className={styles.main}>
-        <Header handleFilteredNotes={handleFilteredNotes} />
-        {modes.editMode && <AddNote />}
-        {filteredNotes}
-      </main>
-    </div>
-  );
-}
+  // Pop warning window size under 1180px width
+  const [windowSize, _setWindowSize] = useState([window.innerWidth, window.innerHeight])
+  const [warningSize, setWarningSize] = useState(false)
 
-export default Home;
+  useEffect(() => {
+    window.innerWidth < 1180 ? setWarningSize(true) : setWarningSize(false)
+    window.addEventListener('resize', () => {
+      window.innerWidth < 1180 ? setWarningSize(true) : setWarningSize(false)
+    })
+  }, [windowSize])
+
+  if (warningSize) {
+    return (
+      <div className={styles.warningSize}>
+        <h1>Votre fenêtre est trop petite ! 🙀😬😮</h1>
+        <br />
+        <p>Note n'est pour le moment pas disponible sur mobile !
+          <br /> Veuillez naviguer depuis un ordinateur* ou une tablette.
+          <br />Ou demander un pc au Père Noël...</p>
+        <br />
+        <p>*Si la page ne s'affiche pas depuis votre ordinateur, <br /> veuillez appuyer sur :
+          <p>Ctrl et - pour Windows et Linux</p>
+          <p>Ctrl et - pour Chrome OS</p>
+          <p>⌘ et - pour Mac</p>
+        </p>
+      </div>
+    )
+  } else {
+
+    return (
+      <div className={`${modes.darkMode && 'darkMode'}`} >
+        <main className={styles.main}>
+          <Header handleFilteredNotes={handleFilteredNotes} />
+          {modes.editMode && <AddNote />}
+          {filteredNotes}
+        </main>
+      </div>
+    );
+  }
+}
